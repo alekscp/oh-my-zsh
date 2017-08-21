2bc0d1b0ffe48f447ced9c5d334d020f3f717597
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-08-21 10:04:49 +0200

    Disable la alias

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -27,7 +27,7 @@ alias d='dirs -v | head -10'
 alias lsa='ls -lah'
 alias l='ls -lah'
 alias ll='ls -lh'
-alias la='ls -lAh'
+# alias la='ls -lAh'
 
 # Push and pop directories on directory stack
 alias pu='pushd'
